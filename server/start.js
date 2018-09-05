b34b8510c86518e41af4592f2772277aca3402f3
NZadeh <neemazad@gmail.com>
2018-09-04 19:45:05 -0700

    Remove leaveRoom from the code.

--- server/start.js
+++ server/start.js
@@ -2,25 +2,31 @@ if (Meteor.isServer) {
     var graceTime = 10*1000; //how long people can leave rooms for
     Meteor.startup(function() {
         Meteor.setInterval(function() {
-            var rooms = GameRooms.find().fetch();
-            rooms.map(function(room) {
-                room.players.map(function(player) {
-                    return player._id;
-                }).map(function(userId) {
-                    var user = Meteor.users.findOne(userId);
-                    var currRoom = user.profile.currentGameRoom;
-                    var timeSinceLeave = +new Date() - user.profile.leftAt;
-                    if (currRoom !== room._id && timeSinceLeave > graceTime) {
-                        //their current room doesn't match this one, so
-                        //remove them from this one
-                        removeJoinAuth(
-                            userId,
-                            user.username,
-                            room._id
-                        );
-                    }
-                });
-            });
+            // TODO(neemazad): This relied on leaveRoom to work. Now we
+            // want to use this to auto-clean-up the rooms if they haven't been
+            // used in a while...
+
+            // Old code below:
+
+            // var rooms = GameRooms.find().fetch();
+            // rooms.map(function(room) {
+            //     room.players.map(function(player) {
+            //         return player._id;
+            //     }).map(function(userId) {
+            //         var user = Meteor.users.findOne(userId);
+            //         var currRoom = user.profile.currentGameRoom;
+            //         var timeSinceLeave = +new Date() - user.profile.leftAt;
+            //         if (currRoom !== room._id && timeSinceLeave > graceTime) {
+            //             //their current room doesn't match this one, so
+            //             //remove them from this one
+            //             removeJoinAuth(
+            //                 userId,
+            //                 user.username,
+            //                 room._id
+            //             );
+            //         }
+            //     });
+            // });
         }, graceTime/3);
     });
 }
