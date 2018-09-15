24d706700b5612f9b46c473b7907068a9bd331a0
NZadeh <neemazad@gmail.com>
2018-09-15 01:04:36 -0700

    Handle footer and header TODOs

--- client/templates/utils/header.js
+++ client/templates/utils/header.js
@@ -4,7 +4,9 @@ Template.header.onRendered(function() {
 
 Template.header.events({
     'click .logout': function() {
-        // TODO(neemazad): Consider removing a player from everything during logout...?
+        // It's nice not to boot players from their state if they log out
+        // and log back in. We will have server-wide clean-up to do that for
+        // us based on timestamp, so we don't need to do anything special here.
         Meteor.logout(function() {
         	FlowRouter.go('home');
         });
