f5853f156d433ed0d0e4509b28b8050169ed40db
NZadeh <neemazad@gmail.com>
2018-09-15 22:45:38 -0700

    Modal buttons red, all buttons closer together

--- client/templates/utils/modal.js
+++ client/templates/utils/modal.js
@@ -1,3 +1,11 @@
 Template.modal.onRendered(function() {
   $('.modal').modal();
 });
+
+Template.modal.helpers({
+  modalAttributes(uniqueId) {
+    return {
+      "data-target": uniqueId,
+    };
+  },
+});
