05994d33ce724acdc919701b5ed874f7f37c67e4
NZadeh <neemazad@gmail.com>
2018-09-15 23:59:14 -0700

    Handle TODO

--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,7 @@ import { GameRooms } from '/lib/collections/game_rooms/game_rooms';
 import { SecretInfo } from '/lib/collections/game_rooms/secret_info';
 import SimpleSchema from 'simpl-schema';
 
-// TODO(neemazad): What is this for?
+// Publish our extended Meteor.users info for use by client-code.
 Meteor.publish('userData', function() {
     return Meteor.users.find({_id: this.userId}, {
         fields: {'currentGameRoom': 1}
